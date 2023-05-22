c79770e644def75896c7e1f7a40294ab76b8b4ad
ERLEY <erley.1701920254@ucaldas.edu.co>
2023-05-22 14:06:15 -0500

    Ajuste en la GUI del AFD Y ANFND

--- controller/AutomataController.py
+++ controller/AutomataController.py
@@ -1,9 +1,12 @@
 from logging import root
+from pathlib import Path
+from matplotlib.patches import PathPatch
 import matplotlib.pyplot as plt
 import tkinter as tk
 import networkx as nx
 
 from matplotlib import pyplot as plt
+import numpy as np
 from model.Automata import *
 from collections import deque
 
@@ -121,8 +124,14 @@ def mostrar_automatas(automata_no_determinista, automata_determinista):
     # Plot non-deterministic automaton
     plt.figure(figsize=(8, 6))
     grafo_no_determinista = nx.DiGraph()
-    grafo_no_determinista.add_edges_from(automata_no_determinista.transiciones)
+    grafo_no_determinista.add_nodes_from(automata_no_determinista.estados)
+    for estado, transiciones in automata_no_determinista.transiciones.items():
+        for entrada, estados_destino in transiciones.items():
+            for estado_destino in estados_destino:
+                grafo_no_determinista.add_edge(
+                    estado, estado_destino, label=entrada)
     pos_no_determinista = nx.spring_layout(grafo_no_determinista, k=0.3)
+
 
     # Colores y tamaños de nodos
     node_colors_no_determinista = [
@@ -158,21 +167,41 @@ def mostrar_automatas(automata_no_determinista, automata_determinista):
     plt.figure(figsize=(8, 6))
     grafo_determinista = nx.DiGraph()
     grafo_determinista.add_nodes_from(automata_determinista.estados)
+
+    frozenset_nodes = []
+
     for estado, transiciones in automata_determinista.transiciones.items():
+        if isinstance(estado, frozenset):  # Verificar si el estado es un frozenset
+            frozenset_nodes.append(estado)
+            for entrada, estados_destino in transiciones.items():
+                estados_frozenset_destino = [e for e in estados_destino if isinstance(e, frozenset)]
+                if estados_frozenset_destino:  # Verificar si hay estados frozenset destino
+                    for estado_destino in estados_frozenset_destino:
+                        grafo_determinista.add_edge(estado, estado_destino, label=entrada)
+    
+    # Obtener las transiciones de los frozensets
+    for frozenset_node in frozenset_nodes:
+        transiciones = automata_determinista.transiciones[frozenset_node]
         for entrada, estados_destino in transiciones.items():
             for estado_destino in estados_destino:
-                grafo_determinista.add_edge(
-                    estado, estado_destino, label=entrada)
-    pos_determinista = nx.spring_layout(grafo_determinista, k=10)
+                if isinstance(estado_destino, frozenset):
+                    for nodo_destino in frozenset_nodes:
+                        if nodo_destino.issubset(estado_destino):
+                            grafo_determinista.add_edge(frozenset_node, nodo_destino, label=entrada)
+
+    # Posiciones de los nodos en el grafo
+    pos_determinista = nx.spring_layout(grafo_determinista, k=0.3)
 
     # Colores y tamaños de nodos
-    node_colors_determinista = ['lightblue' for _ in grafo_determinista.nodes]
+    node_colors_determinista = ['lightblue' if isinstance(node, frozenset) else 'white' for node in grafo_determinista.nodes]
     node_colors_determinista[list(automata_determinista.estados).index(
         automata_determinista.estadoInicial)] = 'green'
     node_colors_determinista[list(automata_determinista.estados).index(
         automata_determinista.estadosFinales[0])] = 'red'
     node_sizes_determinista = [
         1000 if node == automata_determinista.estadoInicial else 800 if node in automata_determinista.estadosFinales else 500 for node in grafo_determinista.nodes]
+
+    # Dibujar el grafo determinista
     nx.draw_networkx(
         grafo_determinista,
         pos_determinista,
@@ -185,14 +214,14 @@ def mostrar_automatas(automata_no_determinista, automata_determinista):
         edge_color='gray',
         alpha=0.7
     )
-    edge_labels_determinista = nx.get_edge_attributes(
-        grafo_determinista, 'label')
-    nx.draw_networkx_edge_labels(grafo_determinista, pos_determinista,
-                                 edge_labels=edge_labels_determinista, font_color='black')
-    plt.title('Autómata determinista')
+
+    # Agregar etiquetas a las transiciones
+    edge_labels = {(u, v): d['label'] for u, v, d in grafo_determinista.edges(data=True)}
+    nx.draw_networkx_edge_labels(grafo_determinista, pos_determinista, edge_labels=edge_labels, font_size=10)
+    
+    plt.title('Autómata Finito Determinista')
     plt.axis('off')
     plt.show()
-
 
 def cargar_automata_json():
     """
